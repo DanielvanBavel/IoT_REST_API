19157a5c51ddc99189913a26c6e7d601859c2141
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2019-04-24 10:47:35 +0200

    Update packages

--- IoT_REST_API_Test/TemperatureSensorControllerTest.cs
+++ IoT_REST_API_Test/TemperatureSensorControllerTest.cs
@@ -1,56 +1,37 @@
 ﻿using IoT_REST_API;
-using IoT_REST_API.Controllers;
-using IoT_REST_API.Models;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
 
 namespace IoT_REST_API_Test
 {
     [TestClass]
     public class TemperatureSensorControllerTest
     {
-        Context _context;
-
         TemperatureSensorControllerTest()
         {
-            InitContext();
+
         }
 
-        public void InitContext()
+        [TestMethod]
+        public void TestGetSensorsWithReturn200Succes()
         {
-            var builder = new DbContextOptionsBuilder<Context>().UseInMemoryDatabase();
+            //Arrange
+            //string expectedTitle = "Sample2";
 
-            var context = new Context(builder.Options);
-            var books = Enumerable.Range(1, 10)
-                .Select(i => new TemperatureSensor { TemperatureSensorId = i, DeviceName = $"Sample{i}", DeviceModel = "Wrox Press" });
-            context.TemperatureSensor.AddRange(books);
-            int changed = context.SaveChanges();
-            _context = context;
+            //Act
+            //var controller = new TemperatureSensorController(_context);
+            //TemperatureSensor result = controller.GetTemperatureSensors();
+
+            //Assert
+            //Assert.IsTrue(Ok());
         }
 
-        //[TestMethod]
-        //public void TestGetSensors()
-        //{
-        //    //Arrange
-        //    string expectedTitle = "Sample2";
-
-        //    //Act
-        //    var controller = new TemperatureSensorController(_context);
-        //    TemperatureSensor result = controller.GetTemperatureSensors();
-
-        //    //Assert
-        //    Assert.AreEqual(expectedTitle, result.DeviceName);
-        //}
-
-        //[TestMethod]
-        //public void TestGetSensorById()
-        //{
+        [TestMethod]
+        public void TestGetSensorByIdWithReturn200Succes()
+        {
         //    string expectedTitle = "Sample2";
         //    var controller = new TemperatureSensorController(_context);
         //    TemperatureSensor result = controller.GetTemperatureSensor(2);
         //    Assert.AreEqual(expectedTitle, result.DeviceName);
-        //}
-
+        }
     }
 }
